4579f1ac4d5dff933ae9c7f49d7a1c0062e5521d
mahendar <mahendarchikkolla@gmail.com>
2023-03-29 05:48:50 +0530

    after modify

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -2,12 +2,24 @@
 const todoList = require("../todo");
 
 const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
+const formattedDate = d => {
+  return d.toISOString().slice(0, 10)
+}
+
+var dateToday = new Date()
+const today = formattedDate(dateToday)
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+)
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1))
+)
 
 describe("Todolist Test Suite", () => {
   beforeAll(() => {
     add({
       title: "Test todo",
-      dueDate: new Date().toISOString().slice(0, 10),
+      dueDate: today,
       completed: false,
     });
   });
@@ -16,7 +28,7 @@ describe("Todolist Test Suite", () => {
     add({
       title: "Test todo",
       completed: false,
-      dueDate: new Date().toISOString().slice(0, 10),
+      dueDate: today,
     });
     expect(all.length).toBe(todoItemsCount + 1);
   });
@@ -28,23 +40,47 @@ describe("Todolist Test Suite", () => {
   });
 
   test("Should retrieve overdue items", () => {
-    const overdueItems = all.filter(
-      (item) => item.dueDate < new Date().toISOString().slice(0, 10)
-    );
-    expect(overdue()).toEqual(overdueItems);
+    // const overdueItems = all.filter(
+    //   (item) => item.dueDate < new Date().toISOString().slice(0, 10)
+    // );
+    const overdueItemsCount = overdue().length;
+    add({
+      title: "Test overDue",
+      dueDate: yesterday,
+      completed: false,
+    });
+    const overdueItems = overdue();
+    expect(overdueItems.length).toBe(overdueItemsCount + 1);
   });
 
   test("Should retrieve today items", () => {
-    const dueTodayItems = all.filter(
-      (item) => item.dueDate === new Date().toISOString().slice(0, 10)
-    );
-    expect(dueToday()).toEqual(dueTodayItems);
+    // const dueTodayItems = all.filter(
+    //   (item) => item.dueDate === new Date().toISOString().slice(0, 10)
+    // );
+    // expect(dueToday()).toEqual(dueTodayItems);
+    const dueTodayItemsCount = dueToday().length;
+    add({
+      title: "Test dueToday",
+      completed: false,
+      dueDate: today,
+    });
+    const dueTodayItems = dueToday();
+    expect(dueTodayItems.length).toBe(dueTodayItemsCount + 1);
   });
 
   test("Should retrieve due later items", () => {
-    const dueLaterItems = all.filter(
-      (item) => item.dueDate > new Date().toISOString().slice(0, 10)
-    );
-    expect(dueLater()).toEqual(dueLaterItems);
-  });
+    //   const dueLaterItems = all.filter(
+    //     (item) => item.dueDate > new Date().toISOString().slice(0, 10)
+    //   );
+    //   expect(dueLater()).toEqual(dueLaterItems);
+    // });
+    const dueLaterItemsCount = dueLater().length;
+    add({
+      title: "Test dueLater",
+      dueDate: tomorrow,
+      completed: false,
+    });
+    const dueLaterItems = dueLater();
+    expect(dueLaterItems.length).toBe(dueLaterItemsCount + 1);
+  })
 });
